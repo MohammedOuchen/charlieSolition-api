4002d00cd0678a9403e58dbd2be7dc2e401e99d0
mohammed ouchen <medouchen19@gmail.com>
2023-07-06 08:47:29 +0200

    fix cors and add routes to api route

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[TrackerController::class, 'dailySensorCounts']);
-Route::get('/charlie',[TrackerController::class, 'geoFencingTrackers']);
+
